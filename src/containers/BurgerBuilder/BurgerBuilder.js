71720f3944201ac6439d1714905433da7ff0c957
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-12-08 16:13:36 -0800

    SideDrawer reusing backdrop, can be closed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxil';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
