931247957c25ba223904c95a5cf11feb30a5af56
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-01-22 14:03:15 -0800

    sends post requests with dummy data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Aux from '../../hoc/Auxil';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   lettuce: 0.25,
@@ -70,7 +71,24 @@ class BurgerBuilder extends Component {
   };
 
   orderContinueHandler = () => {
-    alert('you continue');
+    // alert('you continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Kevin',
+        address: {
+          street: 'test st.',
+          zipCode: '63791',
+          country: 'United States'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   render () {
