2fe108526d8f78aee23720740749199509c37e91
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-01-31 18:55:03 -0800

    pass ingredient data via query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,10 +11,33 @@ class Checkout extends Component {
     }
   };
 
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      console.log(param);
+      // each param is like ["bacon", "0"] etc.
+      ingredients[param[0]] = +param[1];
+      // ingredients['bacon'] = 0 etc.
+    }
+    this.setState({ingredients: ingredients})
+  }
+
+  checkoutCancelHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinueHandler = () => {
+    this.props.history.replace('/checkout/contact-data')
+  };
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancel={this.checkoutCancelHandler}
+          checkoutContinue={this.checkoutContinueHandler}/>
       </div>
     )
   }
