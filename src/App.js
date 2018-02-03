fb4704371181526efee1b327f729285d4cf492b2
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-02-02 16:24:25 -0800

    added orders page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import {Route} from 'react-router-dom';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <div>
         <Layout>
           <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/orders" component={Orders}/>
           <Route path="/checkout" component={Checkout}/>
         </Layout>
       </div>
