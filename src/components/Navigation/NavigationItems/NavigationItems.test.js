aa91ca5a0a0a193557b6763472074eeb901279e0
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-02-24 13:39:03 -0800

    changed test to check if Logout NavigationItem rendered

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -15,8 +15,8 @@ describe('<NavigationItems />', () => {
   it('should render two <NavigationItem /> elements if not authenticated', () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
-  it('should render three <NavigationItem /> elements if authenticated', () => {
+  it('should render <NavigationItem /> Logout element if authenticated', () => {
     wrapper.setProps({isAuthenticated: true});
-    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   });
 });
